0fc3ebed132cdd787f0dd570f4d7f46172b443cd
2hyeonjong <2hyeonjong@gmail.com>
2019-06-27 16:23:10 +0900

    myls

--- blackBox/blackbox.c
+++ blackBox/blackbox.c
@@ -3,15 +3,14 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<string.h>
-
-
+#define SIZE 1024
 int main()
 {
     time_t t =time(NULL);
     struct tm tm = *localtime(&t);
     int year = tm.tm_year+1900, mon = tm.tm_mon+1, day=tm.tm_mday, hour=tm.tm_hour;
     int min=tm.tm_min;
-    char s[1024];
+    char s[SIZE];
     sprintf(s, "%d%d%d_%d%d",year,mon,day,hour,min);
     if(mkdir(s, 0776)==-1){
         printf("error()\n");
